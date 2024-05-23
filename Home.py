f5dac2557cc797b6aef939ae10976613cea6fe47
Bao Pham <huynhbaopham@gmail.com>
2024-05-23 11:32:45 -0400

    rename pages

--- Home.py
+++ Home.py
@@ -56,14 +56,6 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 with st.container(border=True):
-    rows = get_client("6027238354")
-    st.write(rows)
-    # st.page_link(
-    #     "pages/1_Check_In.py", label="Go to Check In", use_container_width=True
-    # )
-    # st.page_link(
-    #     "pages/2_Dashboard.py", label="Go to Dashboard", use_container_width=True
-    # )env
     st.link_button(
         label="Go to Check In",
         url="/Check_In",
